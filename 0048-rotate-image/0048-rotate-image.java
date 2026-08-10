f5de4eaaaad72d0971bc60bcf4621563989fac56
Khushi Mittal <khushimittal2405@gmail.com>
2026-08-11 00:05:15 +0530

    Time: 0 ms (100%), Space: 43.7 MB (71.77%) - LeetHub

---
+++ 0048-rotate-image/0048-rotate-image.java
@@ -0,0 +1,27 @@
+class Solution {
+    public void rotate(int[][] arr) {
+        int m = arr.length;
+        //transpose
+        for(int i=0;i<m;i++){
+            for(int j=0;j<i;j++){
+                int temp = arr[i][j];
+                arr[i][j] = arr[j][i];
+                arr[j][i] = temp;
+            }
+        }
+        //reverse
+        for(int i=0;i<m;i++){
+            int a=0,b=m-1;
+            //swaping
+            while(a<b){
+              
+                int temp= arr[i][a];
+                arr[i][a] = arr[i][b];
+                arr[i][b]=temp;
+                a++;
+                b--;
+
+            }
+        }
+    }
+}
