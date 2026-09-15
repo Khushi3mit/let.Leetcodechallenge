52f7ce842cedeb39e52dae056694e817669328d6
Khushi Mittal <khushimittal2405@gmail.com>
2026-09-15 23:45:20 +0530

    Time: 1 ms (46.91%), Space: 45.9 MB (18.16%) - LeetHub

--- 0832-flipping-an-image/0832-flipping-an-image.java
+++ 0832-flipping-an-image/0832-flipping-an-image.java
@@ -4,7 +4,7 @@ class Solution {
         int[][] ans = new int[m][n];
         for(int i=0;i<m;i++ ){
             for(int j=0;j<n;j++){
-            ans[i][j] = arr[i][n-1-j]; //reverse
+            ans[i][j] = arr[i][n-j-1]; //reverse
             }
         }
       for(int i=0;i<m;i++){
