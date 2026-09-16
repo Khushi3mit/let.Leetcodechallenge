9f975c557ae3d7ee8e590741854c3c6f7a9603b0
Khushi Mittal <khushimittal2405@gmail.com>
2026-09-16 23:39:58 +0530

    Time: 0 ms (100%), Space: 47.5 MB (32.54%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,12 +1,19 @@
 class Solution {
     public int missingNumber(int[] nums) {
-       int n=nums.length;
-       int expectedsum; int actualsum =0;
-       expectedsum = n*(n+1)/2;
-        for(int i=0;i<n;i++){
-  actualsum += nums[i];
-        }
+//        int n=nums.length;
+//        int expectedsum; int actualsum =0;
+//        expectedsum = n*(n+1)/2;
+//         for(int i=0;i<n;i++){
+//   actualsum += nums[i];
+//         }
 
-       return Math.abs(expectedsum - actualsum);
+//        return Math.abs(expectedsum - actualsum);
+// by XOR method
+int n = nums.length;
+int ans=n;
+for(int i=0;i<n;i++){
+    ans= ans ^ i ^ nums[i];
+}
+return ans;
     }
 }
