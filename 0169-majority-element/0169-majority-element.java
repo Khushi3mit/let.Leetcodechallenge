0d9e3a1983184db15963d9d319efdb6a54250581
Khushi Mittal <khushimittal2405@gmail.com>
2026-09-19 12:10:51 +0530

    Time: 2 ms (71.23%), Space: 63.4 MB (5.98%) - LeetHub

--- 0169-majority-element/0169-majority-element.java
+++ 0169-majority-element/0169-majority-element.java
@@ -12,21 +12,39 @@ class Solution {
     //         }
     //     }
     //     return -1;
-    HashMap<Integer,Integer> mpp = new HashMap<>();
-    int n = nums.length;
+
+
+    // HashMap<Integer,Integer> mpp = new HashMap<>();
+    // int n = nums.length;
+    // for(int i=0;i<n;i++){
+    //  int key = nums[i];
+    //  if(mpp.containsKey(key)){
+    //     int freq = mpp.get(key);
+    //     mpp.put(key,freq+1);
+    //  }
+    //  else{
+    //     mpp.put(key,1);
+    //  }
+    //  if(mpp.get(key)>(n/2)){
+    //     return key;
+    //  }
+    // }
+    //      return 0;
+    int  n = nums.length;
+    int freq=0;
+    int majority_ele=0;
     for(int i=0;i<n;i++){
-     int key = nums[i];
-     if(mpp.containsKey(key)){
-        int freq = mpp.get(key);
-        mpp.put(key,freq+1);
-     }
-     else{
-        mpp.put(key,1);
-     }
-     if(mpp.get(key)>(n/2)){
-        return key;
-     }
+        if(freq==0){
+            majority_ele =nums[i];
+            freq=1;
+        }else{
+            if(nums[i]==majority_ele){
+                freq++;
+            }else{
+                freq--;
+            }
+        }
     }
-         return 0;
+      return majority_ele;
     }
 }
